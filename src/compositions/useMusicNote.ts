9ab7fa312549f80ef9017094b7b74d0dd36d1a03
Jason Yu <jason.yu@askattest.com>
2019-11-08 19:10:24 +0000

    add transpose range slider

--- src/compositions/useMusicNote.ts
+++ src/compositions/useMusicNote.ts
@@ -1,9 +1,13 @@
-import { watch, Ref, computed } from '@vue/composition-api';
+import { watch, Ref } from '@vue/composition-api';
 import { playNote, stopNote } from '/audio';
 
-export const useMusicNote = (frequency: number, isPlaying: Ref<boolean>) => {
-  watch(isPlaying, isPlaying => {
-    if (isPlaying) playNote(frequency);
-    else stopNote(frequency);
+export const useMusicNote = (frequency: Ref<number>, isPlaying: Ref<boolean>) => {
+  watch(() => {
+    if (isPlaying.value) playNote(frequency.value);
+    else stopNote(frequency.value);
+  });
+
+  watch(frequency, (_frequency, oldFrequency) => {
+    stopNote(oldFrequency);
   });
 };
